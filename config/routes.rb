35fc22ffb9391ceb20c5eb9080ccb26e85b5a022
Charles Lee <chug2k@gmail.com>
2015-11-24 15:18:54 +0700

    adding base route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'welcome/index'
+  root 'welcome#index'
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
